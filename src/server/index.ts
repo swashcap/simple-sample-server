78e33d4ba97a9cb262c560cfe6d9c437d1fb51e3
Cory Reed <swashcap@gmail.com>
2019-04-09 02:00:06 -0700

    feat: add favicon

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,6 +6,7 @@ import cors from 'cors'
 import express from 'express'
 import morgan from 'morgan'
 import errorhandler from 'errorhandler'
+import path from 'path'
 
 import { ssr } from './ssr'
 
@@ -22,6 +23,7 @@ if (env === 'development' || env === 'test') {
   app.use(errorhandler())
 }
 
+app.use(express.static(path.resolve(__dirname, '../../public')))
 app.get('*', ssr())
 
 if (require.main === module) {
