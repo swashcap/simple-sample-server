4359a570004bcaf97b63222839146fd68794a95d
Cory Reed <swashcap@gmail.com>
2019-04-09 01:35:52 -0700

    refactor: use common text throughout components

--- src/client/components/Button.tsx
+++ src/client/components/Button.tsx
@@ -1,6 +1,8 @@
 import { h } from 'preact'
 import clsx from 'clsx'
 
+import { Text } from './text/Text'
+
 export interface ButtonProps extends JSX.HTMLAttributes {
   element: keyof JSX.IntrinsicElements
   variant: 'full' | 'hollow'
@@ -8,13 +10,14 @@ export interface ButtonProps extends JSX.HTMLAttributes {
 
 export const Button = ({
   class: className,
+  children,
   element: Component = 'button',
   variant = 'full',
   ...rest
 }: ButtonProps) => (
   <Component
     class={clsx(
-      'ba b--dark-green br2 dib dim f5 link pointer ph3 pv2',
+      'ba b--dark-green br2 dib dim link pointer ph3 pv1',
       {
         'bg-dark-green white': variant === 'full',
         'bg-white dark-green': variant !== 'full'
@@ -22,5 +25,7 @@ export const Button = ({
       className
     )}
     {...rest}
-  />
+  >
+    <Text>{children}</Text>
+  </Component>
 )
