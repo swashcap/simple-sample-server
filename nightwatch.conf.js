a1bb1aa4209c5bc9d7881a77573f12920da9ffad
Cory Reed <swashcap@gmail.com>
2019-05-22 22:57:13 -0700

    chore: add selenium config

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,11 +1,16 @@
+const chromedriver = require('chromedriver')
+const seleniumServer = require('selenium-server')
 module.exports = {
-  src_folders: ['tests'],
-
-  webdriver: {
+  selenium: {
     start_process: true,
-    server_path: 'node_modules/.bin/chromedriver',
-    port: 9515
+    server_path: seleniumServer.path,
+    log_path: '',
+    port: 4444,
+    cli_args: {
+      'webdriver.chrome.driver': chromedriver.path
+    }
   },
+  src_folders: ['tests'],
 
   test_settings: {
     default: {
